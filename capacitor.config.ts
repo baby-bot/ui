fd085f430c7b3d3bcb074ac6a819a446394f29e2
Kevin Towe <kevintowe27@gmail.com>
2024-01-28 19:20:43 -0600

    added firebase config for ios

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,12 +1,12 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'io.baby-bot.ui',
   appName: 'baby-bot-ui',
   webDir: 'www',
   server: {
-    androidScheme: 'https'
-  }
+    androidScheme: 'https',
+  },
 };
 
 export default config;
